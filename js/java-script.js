beae4bbbd790e6a1df5be56a629362cbd1eb8930
Anieb22 <anielloverdino@gmail.com>
2023-05-17 17:15:56 +0200

    aggiunto javascript (esercizio concluso)

---
+++ js/java-script.js
@@ -0,0 +1,25 @@
+let km_percorsi = parseInt(prompt('Quanti chilometri dovrai percorrere?'))
+let eta = parseInt(prompt('Quanti anni hai?'))
+let prezzo = (km_percorsi * 0.21)
+let prezzo_finale = prezzo.toFixed(2)
+
+if (isNaN(km_percorsi) || isNaN(eta)){
+    alert('il carattere inserito non è un numero');
+}
+
+else if (eta < 18){
+    (prezzo * 0.8)
+    console.log(prezzo)
+}
+
+else if (eta > 65){
+    (prezzo * 0.6)
+    console.log(prezzo)
+}
+
+else{
+    prezzo
+    console.log(prezzo)
+}
+
+document.getElementById('biglietto').innerHTML= prezzo_finale

edc6f3a9f8a7ba6a691515321ff3c9881a1c6bcf
Anieb22 <anielloverdino@gmail.com>
2023-05-18 17:22:58 +0200

    hotfix java script

--- js/java-script.js
+++ js/java-script.js
@@ -1,19 +1,21 @@
 let km_percorsi = parseInt(prompt('Quanti chilometri dovrai percorrere?'))
 let eta = parseInt(prompt('Quanti anni hai?'))
 let prezzo = (km_percorsi * 0.21)
-let prezzo_finale = prezzo.toFixed(2)
+
 
 if (isNaN(km_percorsi) || isNaN(eta)){
     alert('Uno dei due caratteri non è un numero');
 }
 
 else if (eta < 18){
-    (prezzo * 0.8)
+    prezzo = prezzo * 0.8
+    prezzo = prezzo.toFixed(2)
     console.log(prezzo)
 }
 
 else if (eta > 65){
-    (prezzo * 0.6)
+    prezzo = prezzo * 0.6
+    prezzo = prezzo.toFixed(2)
     console.log(prezzo)
 }
 
@@ -22,4 +24,4 @@ else{
     console.log(prezzo)
 }
 
-document.getElementById('biglietto').innerHTML= prezzo_finale
+document.getElementById('biglietto').innerHTML= prezzo
